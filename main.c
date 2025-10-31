69f2f478bd49c854f00dfaa1e03872aa0500924d
shreyasriram <31128681+shreyasriram@users.noreply.github.com>
2025-10-31 09:42:46 -0700

    Add main function to read and print buffer content
    Implement main function to read input and display it.

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char buffer[10];  
+    
+    if (argc < 2) {
+        printf("Usage: %s <input>\n", argv);
+        return 1;
+    }
+    gets(buffer); 
+    printf("Buffer content: %s\n", buffer);
+    
+    return 0;
+}
