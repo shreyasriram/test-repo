c0d0858f30113a520532213c4eda70295c5ec95a
shreyasriram <31128681+shreyasriram@users.noreply.github.com>
2025-11-03 14:36:31 -0800

    Pervaziv AI: Bug Fix

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
     char buffer[10];  
@@ -8,7 +9,7 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s <input>\n", argv);
         return 1;
     }
-    gets(buffer); 
+    fgets(buffer, sizeof(buffer), stdin); 
     printf("Buffer content: %s\n", buffer);
     
     return 0;
